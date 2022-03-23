0a765e8ead094b49aabe8efb17b3c535de819f11
Edswel <franckini@yahoo.com>
2022-03-23 11:01:28 +0100

    Implement register/login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
 
 function App() {
     return (
-        <h1 className="text-3xl font-bold underline">Welcome!</h1>
+        <Routes>
+            <Route path='login' element={<Login />} />
+            <Route path='/*' element={<Home />} />
+        </Routes>
     )
 }
 
