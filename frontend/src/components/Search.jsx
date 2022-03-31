055011deecca23d97fbaa7576e34a323df1b214e
Edswel <franckini@yahoo.com>
2022-03-31 10:42:13 +0100

    Implement search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
-function Search() {
+function Search({ searchTerm }) {
+    const [pins, setPins] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if (searchTerm) {
+            setLoading(true);
+            const query = searchQuery(searchTerm.toLowerCase());
+            client.fetch(query)
+                .then((data) => {
+                    setPins(data);
+                    setLoading(false);
+                })
+        } else {
+            client.fetch(feedQuery)
+                .then((data) => {
+                    setPins(data);
+                    setLoading(false);
+                })
+        }
+    }, [searchTerm]);
+
     return (
-        <div>Search</div>
+        <div>
+            {loading && <Spinner message='Searching for posts...' />}
+            {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+            {pins?.length === 0 && searchTerm !== '' && !loading && (
+                <div className='mt-10 text-center text-xl'>No Posts Found!!!</div>
+            )}
+        </div>
     )
 }
 
